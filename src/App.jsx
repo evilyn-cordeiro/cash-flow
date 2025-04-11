a83080ef5e302f165a3f2f1dbc5fa5e7d058b146
evilyn-cordeiro <evilyndeveloper@gmail.com>
2025-04-11 19:21:48 -0300

    feat: criação de rotas para receber as páginas e o fluxo de telas corretamente utilizando react router DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AgendamentoCliente from "./pages/AgendamentoCliente";
 
 function App() {
   return (
-    <>
-      <AgendamentoCliente />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<div> home</div>} />
+        <Route path="/agendamento" element={<AgendamentoCliente />} />
+        <Route path="/contato" element={<div>contato</div>} />
+      </Routes>
+    </Router>
   );
 }
 
